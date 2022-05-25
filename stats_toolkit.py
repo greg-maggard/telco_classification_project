38430279e7be5fc7d08cd9a256593d28fa279e09
Greg Maggard <gregamaggard@gmail.com>
2022-05-25 12:52:31 -0500

    Made more refinements on final report, added stats_toolkit module.

---
+++ stats_toolkit.py
@@ -0,0 +1,10 @@
+def significance_test(p):
+    '''
+    Assumes an alpha of .05. Takes in a value, p, and returns a string stating whether or not that p value indicates sufficient evidence
+    to reject a null hypothesis.
+    '''
+    α = 0.05
+    if p < α:
+        print("Sufficient evidence -> Reject the null hypothesis.")
+    else:
+        print("Insufficient evidence -> Fail to reject the null hypothesis.")
