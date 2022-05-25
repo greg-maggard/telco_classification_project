c17515a25295bc6eb52f20f46d3ee7bfa8639efa
Greg Maggard <gregamaggard@gmail.com>
2022-05-24 23:40:12 -0500

    Made progress on project and moved it to relevant repository.

---
+++ acquire.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import os
+from env import get_db_url
+
+def get_titanic_data():
+    filename = "titanic.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename, index_col = 0)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('SELECT * FROM passengers', get_db_url('titanic_db'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df
+
+def get_iris_data():
+    filename = "iris.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename, index_col = 0)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('''SELECT species_name, measurements.* FROM species 
+                        JOIN measurements ON species.species_id = measurements.species_id;''', get_db_url('iris_db'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df
+
+def get_telco_data():
+    filename = "telco_churn.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename, index_col = 0)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('''SELECT * FROM customers
+                            JOIN contract_types ON customers.contract_type_id = contract_types.contract_type_id
+                            JOIN payment_types ON customers.payment_type_id = payment_types.payment_type_id
+                            JOIN internet_service_types ON customers.internet_service_type_id = internet_service_types.internet_service_type_id;''', get_db_url('telco_churn'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df
---
+++ prepare.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+# train test split from sklearn
+from sklearn.model_selection import train_test_split
+# imputer from sklearn
+from sklearn.impute import SimpleImputer
+
+#Clean Titanic Data Files:
+
+def clean_titanic_data(df):
+    '''
+    Takes in a titanic dataframe and returns a cleaned dataframe
+    Arguments: df - a pandas dataframe with the expected feature names and columns
+    Returns: clean_df - a dataframe with the cleaning operations performed on it
+    '''
+    #Drop Duplicates
+    df.drop_duplicates(inplace = True)
+    #Drop Columns
+    columns_to_drop = ['embarked', 'class', 'passenger_id', 'deck']
+    df = df.drop(columns = columns_to_drop)
+    #Encoded Categorical Variables
+    dummy_df = pd.get_dummies(df[['sex', 'embark_town']], dummy_na = False, drop_first = [True, True])
+    df = pd.concat([df, dummy_df], axis = 1)
+    return df.drop(columns =  ['sex', 'embark_town'])
+
+def impute_age(train, validate, test):
+    '''
+    Imputes the mean age of train to all three datasets.
+    '''
+    imputer = SimpleImputer(strategy = 'mean', missing_values = np.nan)
+    imputer = imputer.fit(train[['age']])
+    train[['age']] = imputer.transform(train[['age']])
+    validate[['age']] = imputer.transform(validate[['age']])
+    test[['age']] = imputer.transform(test[['age']])
+    return train, validate, test
+
+def prep_titanic_data(df):
+    df = clean_titanic_data(df)
+    train, test = train_test_split(df, train_size = 0.8, stratify = df.survived, random_state = 1234)
+    train, validate = train_test_split(train, train_size = .7, stratify = train.survived, random_state = 1234)
+    train, validate, test = impute_age(train, validate, test)
+    return train, validate, test
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+#Cleaning Iris Data:
+def clean_iris_data(df):
+    #Dropping unneeded columns
+    columns_to_drop = ['species_id', 'measurement_id']
+    df = df.drop(columns = columns_to_drop)
+    #renaming species_name column
+    df = df.rename(columns = {'species_name' : 'species'})
+    #creating dummy variables of species
+    dummy_df = pd.get_dummies(df[['species']], drop_first = True)
+    #concatenating dummy variables onto original dataframe
+    df = pd.concat([df, dummy_df], axis = 1)
+    return df
+
+def prep_iris_data(df):
+    df = clean_iris_data(df)
+    train, test = train_test_split(df, train_size = 0.8, stratify = df.species, random_state = 1234)
+    train, validate = train_test_split(train, train_size = 0.8, stratify = train.species, random_state = 1234)
+    return train, validate, test
+
+    #--------------------------------------------------------------------------------------------------------------------------------------------
+
+#Prepare Telco Data:
+def clean_telco_data(df):
+    #Replacing empty cells with nulls:
+    df = df.replace(' ', np.nan)
+    #Replacing ints with yes or no:
+    df.senior_citizen = df.senior_citizen.replace([0, 1], ['No', 'Yes'])
+    #Changing total_charges from 'object' type to 'float64':
+    df.total_charges = df.total_charges.astype('float')
+    #Dropping rows with nulls:
+    df = df.dropna()
+    #Dropping unneeded columns:
+    columns_to_drop = ['contract_type_id', 'payment_type_id', 'internet_service_type_id']
+    df = df.drop(columns = columns_to_drop)
+    #creating dummy variables of categorical columns
+    dummy_df = pd.get_dummies(df[['gender', 'senior_citizen', 'partner', 'dependents', 'phone_service', 'multiple_lines', 'online_security', 'online_backup', 'device_protection', 'tech_support', 'streaming_tv', 'streaming_movies', 'paperless_billing', 'contract_type', 'payment_type', 'internet_service_type']], drop_first = True)
+    #concatenating dummy variables onto original dataframe
+    df = pd.concat([df, dummy_df], axis = 1)
+    return df
+
+def prep_telco_data(df):
+    df = clean_telco_data(df)
+    train, test = train_test_split(df, train_size = 0.8, stratify = df.churn, random_state = 1234)
+    train, validate = train_test_split(train, train_size = 0.8, stratify = train.churn, random_state = 1234)
+    return train, validate, test
+
